8ed6d641bc61634dc0b7e29f008781fe3cfac07c
_space <60070162+spaceman023@users.noreply.github.com>
2022-07-08 22:10:06 -0500

    Time: 193 ms (64.12%), Space: 50.5 MB (83.67%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
@@ -5,34 +5,26 @@
 var pacificAtlantic = function(heights) {
     let rows = heights.length;
     let cols = heights[0].length;
-    function dfs(node, result = []){
+    function dfs(node, result){
       let entry = createEntry(node)
-      if (result.includes(entry)) return result
-      result.push(entry)
+      if (result.has(entry)) return
+      result.add(entry)
       let [r,c] = node;
       if (r + 1 < rows && heights[r+1][c] >= heights[r][c]) dfs([r+1,c], result)
       if (r-1 >= 0 && heights[r-1][c] >= heights[r][c]) dfs([r-1, c], result)
       if (c+1 < cols && heights[r][c+1] >= heights[r][c]) dfs([r, c+1], result)
       if (c-1 >= 0 && heights[r][c-1] >= heights[r][c]) dfs([r, c-1], result)
-      return result
+      return
     }
     let pacific = new Set()
     let atlantic = new Set()
     for (let r = 0; r < rows; r++){
-      for (let e of dfs([r,0])){
-        pacific.add(e)
-      }
-      for (let e of dfs([r, cols-1])){
-        atlantic.add(e)
-      }
+      dfs([r,0], pacific)
+      dfs([r, cols-1], atlantic)
     }
     for (let c = 0; c < cols; c++){
-      for (let e of dfs([0,c])) {
-        pacific.add(e)
-      }
-      for (let e of dfs([rows-1, c])){
-        atlantic.add(e)
-      }
+      dfs([0,c], pacific)
+      dfs([rows-1, c], atlantic)
     }
   let common = [...pacific].filter(i => atlantic.has(i)).map(i => i.split(",").map(i => Number(i)))
   return common
