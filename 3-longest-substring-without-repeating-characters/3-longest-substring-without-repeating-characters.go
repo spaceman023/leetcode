338ff0f8086a3b1d5ff94415423fb117ee383eca
_space <60070162+spaceman023@users.noreply.github.com>
2022-07-15 22:47:31 -0500

    Time: 46 ms (25.65%), Space: 3.5 MB (34.82%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.go
@@ -11,12 +11,17 @@ func lengthOfLongestSubstring(s string) int {
 
 	for i := 0; i < len(s); i++ {
 		if strings.Contains(current, string(s[i])) {
-			if len(current) > longest {
+       if len(current) > longest {
 				longest = len(current)
 			}
-			i -= len(current) - 1
-			current = string(s[i])
-
+      for j:=i-len(current); j < i; j++ {
+        if s[j] == s[i] {
+          current = s[j+1:i+1]
+          fmt.Println(current)
+        }
+      }
+     
+      
 		} else {
 			current = current + string(s[i])
 		}
