70d78c71edf1c43ab28e51c48a3d048d1122086c
_space <60070162+spaceman023@users.noreply.github.com>
2022-07-16 14:21:52 -0500

    Time: 302 ms (7.94%), Space: 51.9 MB (52.38%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.js
@@ -0,0 +1,24 @@
+const findPaths = (rows, cols, moves, x, y) => {
+  const M = 1000000000 + 7
+  let dp = new Array(rows).fill(0).map(i => new Array(cols).fill(0))
+  console.log(dp)
+  dp[x][y] = 1;
+  let count = 0;
+  for (let m = 1; m <= moves; m++) {
+    let temp = new Array(rows).fill(0).map(i => new Array(cols).fill(0))
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        if (i == rows - 1) count = (count + dp[i][j]) % M;
+        if (j == cols - 1) count = (count + dp[i][j]) % M;
+        if (i == 0) count = (count + dp[i][j]) % M;
+        if (j == 0) count = (count + dp[i][j]) % M;
+        temp[i][j] = (
+          ((i > 0 ? dp[i - 1][j] : 0) + (i < rows - 1 ? dp[i + 1][j] : 0)) % M +
+          ((j > 0 ? dp[i][j - 1] : 0) + (j < cols - 1 ? dp[i][j + 1] : 0)) % M
+        ) % M
+      }
+    }
+      dp = temp;
+  }
+  return count
+}
