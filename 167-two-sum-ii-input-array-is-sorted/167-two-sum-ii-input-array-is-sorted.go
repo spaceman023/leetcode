ae1cd83e98fc6c66a4e0bdaf711114e105f62f9f
_space <60070162+spaceman023@users.noreply.github.com>
2022-07-15 22:01:30 -0500

    Time: 10 ms (92.06%), Space: 5.3 MB (95.09%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,16 @@
+func twoSum(numbers []int, target int) []int {
+    i := 0 
+    j := len(numbers) - 1
+    for {
+        sum := numbers[i] + numbers[j]
+        if sum == target {
+            return []int{i+1, j+1} 
+        }
+        if sum > target {
+            j--
+        }
+        if sum < target {
+            i++
+        }
+    }
+}
