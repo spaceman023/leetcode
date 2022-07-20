d813fb725a2fc930c2fc6ae91168630ba29577ca
_space <60070162+spaceman023@users.noreply.github.com>
2022-07-19 22:50:46 -0500

    Time: 82 ms (61.90%), Space: 42.2 MB (34.40%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -7,8 +7,9 @@ var numIdenticalPairs = function(nums) {
     let pairs = 0;
     for (let i = 0; i < nums.length; i++){
       if (seen.has(nums[i])){
-        pairs+=seen.get(nums[i]);
-        seen.set(nums[i], seen.get(nums[i])+1);
+        let curr = seen.get(nums[i])
+        pairs+=curr
+        seen.set(nums[i], curr+1);
       } else {
         seen.set(nums[i], 1)
       }
