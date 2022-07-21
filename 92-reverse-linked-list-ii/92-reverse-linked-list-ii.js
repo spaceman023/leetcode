3397e5e6508fa7331210d6f103901935407fdb94
_space <60070162+spaceman023@users.noreply.github.com>
2022-07-20 20:22:55 -0500

    Time: 77 ms (66.06%), Space: 42.5 MB (21.72%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.js
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.js
@@ -12,32 +12,32 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, m, n) {
-        if (head == null) {
-            return null;
-        }
-        let cur = head
-        let prev = null;
-        while (m > 1) {
-            prev = cur;
-            cur = cur.next;
-            m--;
-            n--;
-        }
-        let con = prev
-        let tail = cur;
-        let third = null;
-        while (n > 0) {
-            third = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = third;
-            n--;
-        }
-        if (con != null) {
-            con.next = prev;
-        } else {
-            head = prev;
-        }
-        tail.next = cur;
-        return head;
+    if (head == null) {
+        return null;
+    }
+    let cur = head
+    let prev = null;
+    while (m > 1) {
+        prev = cur;
+        cur = cur.next;
+        m--;
+        n--;
+    }
+    let con = prev
+    let tail = cur;
+    let third = null;
+    while (n > 0) {
+        third = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = third;
+        n--;
+    }
+    if (con != null) {
+        con.next = prev;
+    } else {
+        head = prev;
+    }
+    tail.next = cur;
+    return head;
 };
