b88fc837dff771d841d5cfe86d76057e26fc054e
_space <60070162+spaceman023@users.noreply.github.com>
2022-07-24 20:17:53 -0500

    Time: 0 ms (100.00%), Space: 7.7 MB (47.09%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(std::vector<int>& nums) {
+        int rob1 = 0;
+        int rob2 = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            int temp = std::max(nums[i] + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        return rob2;
+  }
+
+};
