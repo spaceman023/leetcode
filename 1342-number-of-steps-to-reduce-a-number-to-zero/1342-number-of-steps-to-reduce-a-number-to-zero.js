333dabeae00f37d9d66d283f3edf05a690da4b81
_space <60070162+spaceman023@users.noreply.github.com>
2022-08-05 19:28:26 -0500

    Time: 79 ms (68.50%), Space: 41.5 MB (97.28%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count = 0;
+    while (num > 0){
+        count++
+        if (num % 2 == 0){
+            num/=2
+        } else {
+            num-=1
+        }
+    }
+    return count;
+};
