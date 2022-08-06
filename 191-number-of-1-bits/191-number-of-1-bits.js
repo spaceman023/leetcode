dc4ff21a865e7e5d80d55e81eee6ab715fa904e3
_space <60070162+spaceman023@users.noreply.github.com>
2022-08-06 08:47:54 -0500

    Time: 73 ms (87.67%), Space: 42.7 MB (42.47%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+    while (n > 0){
+        if (n % 2 == 1) count++
+        n = n >>> 1
+    }
+    return count
+};
