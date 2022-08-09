ec0e055858cc8b4247e922705bcfa34efb74562c
_space <60070162+spaceman023@users.noreply.github.com>
2022-08-08 20:42:56 -0500

    Time: 131 ms (80.70%), Space: 49.5 MB (42.30%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -1,40 +1,35 @@
+
 /**
+ * https://leetcode.com/problems/merge-intervals/
+ * Time O(N * logN) | Space O(N)
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
-    let ret = []
-    let parr = null
-    intervals.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i < intervals.length; i++){
-        let [a,b] = intervals[i];
-        if (!parr){
-            let c, d
-            if (i < intervals.length-1) {
-                [c,d] = intervals[i+1];
-            } else {
-                return [...ret, intervals[i]]
-            }
-            if (c > b){
-                ret.push(intervals[i])
-            } else {
-                parr = [Math.min(a,c), Math.max(b, d)];
-            }
-        } else {
-            let [pa, pb] = parr;
-            if (pb < a){
-                ret.push(parr);
-                i--
-                parr = null;
-            } else if (
-                pb <= b
-            ) {
-                parr = [Math.min(a, pa), Math.max(b, pb)]
-            }
-        }
-    }
-    if (parr) {
-        ret.push(parr)
-    }
-    return ret
+ var merge = function(intervals) {
+    intervals.sort(([ aStart, aEnd ], [ bStart, bEnd ]) => aStart !== bStart
+        ? aStart - bStart
+        : aEnd - bEnd
+    );
+
+    return mergerInterval(intervals)
 };
+
+const mergerInterval = (intervals, merged = []) => {
+    let prev = intervals.shift();
+
+    for (const curr of intervals) {
+        const [ prevStart, prevEnd ] = prev;
+        const [ currStart, currEnd ] = curr;
+
+        const hasOverlap = currStart <= prevEnd;
+        if (hasOverlap) {
+            prev[1] = Math.max(prev[1], curr[1]);
+            continue;
+        }
+
+        merged.push(prev);
+        prev = curr;
+    }
+
+    return [ ...merged, prev ];
+}
