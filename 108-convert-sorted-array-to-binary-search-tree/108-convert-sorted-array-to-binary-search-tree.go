7a12182892fb36a4e811aac9311240a87b5c0125
_space <60070162+spaceman023@users.noreply.github.com>
2022-08-09 20:35:56 -0500

    Time: 0 ms (100.00%), Space: 3.5 MB (76.51%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.go
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.go
@@ -8,16 +8,12 @@
  */
 
 func sortedArrayToBST(nums []int) *TreeNode {
-    return doWork(nums)
-}
-
-func doWork(nums []int) *TreeNode {
     if len(nums) < 1 {
         return nil
     }
     M := int(math.Floor(float64(len(nums)/2)))
     root := TreeNode{nums[M], nil, nil}
-    root.Right = doWork(nums[M+1:])
-    root.Left = doWork(nums[0:M])
+    root.Right = sortedArrayToBST(nums[M+1:])
+    root.Left = sortedArrayToBST(nums[0:M])
     return &root
 }
