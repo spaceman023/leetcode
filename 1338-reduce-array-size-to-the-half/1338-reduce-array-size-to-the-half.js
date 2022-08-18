981123c348703a6ba09bf36a02fdb8f480b49494
_space <60070162+spaceman023@users.noreply.github.com>
2022-08-17 20:07:59 -0500

    Time: 370 ms (11.32%), Space: 115.5 MB (5.66%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var minSetSize = function(arr) {
+    let dic = {};
+    for (let i = 0; i < arr.length; i ++){
+        dic[arr[i]] = (dic[arr[i]] || 0) + 1        
+    }
+    let steps = []
+    for (let [k,v] of Object.entries(dic)){
+        steps.push([k, v])
+    }
+    steps.sort((a,b) => b[1] - a[1])
+    let length = arr.length
+    let goal = arr.length/2
+    let count = 0
+    console.log(steps)
+    while (length > goal){
+        count++
+        length -= steps[count-1][1]
+        if (length < goal){
+            break;
+        }
+    }
+    return count
+};
