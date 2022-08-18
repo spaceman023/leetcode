f77d4ffa225476926c83af7e4b0f6947c06f0502
_space <60070162+spaceman023@users.noreply.github.com>
2022-08-17 21:24:40 -0500

    Time: 5567 ms (6.45%), Space: 54.8 MB (60.85%) - LeetHub

---
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let max = -Infinity
+    if (nums.length <= k){
+        return nums.reduce((a,b) => a+b)/nums.length
+    }
+    for (let i = 0; i <= nums.length-k; i++){
+        let s = nums[i]
+        for (let j = i+1; j < i+k; j++){
+            s+=nums[j]
+        }
+        if (s/k > max){
+            max = s/k
+        }
+    }
+    return max
+};
